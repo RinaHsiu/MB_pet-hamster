f2f7bd300a28567f6f0525d73cba370e05e4403e
RinaHsiu <rina.shanxu@outlook.com>
2024-10-24 11:03:53 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onGesture(Gesture.Shake, function on_gesture_shake() {
+    basic.showIcon(IconNames.Sad)
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Dadadadum), music.PlaybackMode.InBackground)
+    basic.showIcon(IconNames.Asleep)
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function on_logo_pressed() {
+    basic.showIcon(IconNames.Happy)
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Entertainer), music.PlaybackMode.UntilDone)
+    basic.showIcon(IconNames.Asleep)
+})
+basic.showIcon(IconNames.Asleep)
